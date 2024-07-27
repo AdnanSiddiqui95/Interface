e3f814157dcbd50006b537ec342f6ebf440f6118
Adnan Siddiqui <sid.adnan9569@gmail.com>
2024-07-27 22:27:04 +0530

    Add files via upload

---
+++ BBClass.java
@@ -0,0 +1,11 @@
+public class BBClass implements BB ,BB1
+{
+	public void method1()
+	{
+		System.out.println("This is method of BB1 interface");
+	}
+	public void method2()
+	{
+		System.out.println("This is the method of BB2 interface");
+	}
+}
